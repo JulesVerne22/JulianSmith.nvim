630062e04ca372494bd00d5f916a905f5104f8a2
Julian Smith <juliansmith@mitre.org>
2023-07-31 10:49:17 -0500

    Fixed toggleterm escape keybinding

--- init.lua
+++ init.lua
@@ -536,11 +536,11 @@ vim.api.nvim_set_keymap('n', '<leader>e', '<cmd>NvimTreeToggle<CR>', { desc = 'O
 -- [[ Configure ToggleTerm ]]
 function _G.set_terminal_keymaps()
   local opts = { buffer = 0 }
-  vim.keymap.set('t', '<C-]', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', '<C-[>', [[<C-\><C-n>]], opts)
   vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
 end
 
-vim.cmd 'autocmd! TermOpen term://* lua set_terminal_keymaps()'
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
 
 local Terminal = require('toggleterm.terminal').Terminal
 
