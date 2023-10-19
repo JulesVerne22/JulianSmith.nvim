4e9bba7fec8ccdc7141f22e1d98998fec4b28731
Julian Smith <juliansmith@mitre.org>
2023-10-19 13:33:49 -0500

    Fixed indentation issue with version 3

--- init.lua
+++ init.lua
@@ -153,9 +153,9 @@ require('lazy').setup({
     'lukas-reineke/indent-blankline.nvim',
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help indent_blankline.txt`
+    main = "ibl",
     opts = {
-      char = '┇',
-      show_trailing_blankline_indent = false,
+      indent = { char = '┇' },
     },
   },
 
