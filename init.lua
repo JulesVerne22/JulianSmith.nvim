443e805169361d41a1790805d40abea8d4773107
Julian Smith <juliari114@gmail.com>
2023-11-21 20:23:41 -0600

    Dynamic nvim tree size

--- init.lua
+++ init.lua
@@ -19,11 +19,6 @@ function dump(o)
   end
 end
 
-local gwidth = vim.api.nvim_list_uis()[1].width
-local gheight = vim.api.nvim_list_uis()[1].height
-local height = math.floor(gheight * 0.8)
-local width = math.floor(gwidth * 0.8)
-
 -- Install package manager
 --    https://github.com/folke/lazy.nvim
 --    `:help lazy.nvim.txt` for more info
@@ -292,14 +287,21 @@ require('lazy').setup({
           width = 40,
           float = {
             enable = true,
-            open_win_config = {
-              border = 'rounded',
-              relative = 'editor',
-              width = width,
-              height = height,
-              row = (gheight - height) * 0.3,
-              col = (gwidth - width) * 0.5,
-            },
+            open_win_config = function()
+              local gwidth = vim.api.nvim_list_uis()[1].width
+              local gheight = vim.api.nvim_list_uis()[1].height
+              local width = math.floor(gwidth * 0.8)
+              local height = math.floor(gheight * 0.8)
+
+              return {
+                border = 'rounded',
+                relative = 'editor',
+                width = width,
+                height = height,
+                row = (gheight - height) * 0.3,
+                col = (gwidth - width) * 0.5,
+              }
+            end,
           },
         },
         filters = {
